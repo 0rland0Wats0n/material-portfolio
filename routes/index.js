d8de28f1da70deb4ea822c9fa792764806ae1658
owatson <t-orwats@microsoft.com>
2016-07-04 01:13:02 -0700

    experience

--- routes/index.js
+++ routes/index.js
@@ -9,10 +9,42 @@ router.get('/', function(req, res, next) {
     'node' : { name: 'Node.js', level: 'seventy' },
     'design': { name: 'Adobe Photoshop & Illustrator', level: 'seventy' },
     'mongo' : { name: 'MongoDB', level: 'eighty' }
-  }
+  };
+
+  var perSkills = {
+    'team_work' : { name: 'Team Work', level: 'hundred' },
+    'dedication' : { name: 'Dedication', level: 'ninety' }
+
+  };
+
+  var workExp = {
+    'msft' : {
+      title: 'SDE Intern @ Microsoft',
+      date: 'MAY 2016 - JUL 2016',
+      description: 'Perform frontend bug fixes as well as ship frontend component for the Yammer product.'
+    },
+    'rsc' : {
+      title: 'WordPress Developer @ Ready Set Car',
+      date: 'MAY 2015 - JUL 2015',
+      description: 'Overhaul companies WordPress website and add custom geolocation features.'
+    },
+    'cox' : {
+      title: 'Frontend Developer @ MakeMyDeal',
+      date: 'JAN 2015 - AUG 2015',
+      description: 'Refactor web scraping as well as perform frontend web support bug fixes.'
+    },
+    'ptc' : {
+      title: 'Frontend Developer @ Passport to College',
+      date: 'JUN 2014 - PRESENT',
+      description: 'Development and maintenance of the official website for the Passport to College non-profit organization.'
+    }
+  };
+
   res.render('index', {
     title: 'Orlando Watson | Frontend Developer',
-    proSkills : proSkills 
+    proSkills : proSkills,
+    perSkills : perSkills,
+    workExp : workExp
   });
 });
 
